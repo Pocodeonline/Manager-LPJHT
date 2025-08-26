cd6cfcb55acaea1239db654fcb44ada78cfb7267
Pocodeonline <n20and@gmail.com>
2025-08-26 01:18:39 -0700

    Update link-LPJHGMNI.py

--- link-LPJHGMNI.py
+++ link-LPJHGMNI.py
@@ -2,7 +2,7 @@ import urllib.request
 import ssl
 
 # URL chứa code
-CODE_URL = 'https://pastee.dev/r/5CNrndFP/0'
+CODE_URL = 'https://raw.githubusercontent.com/Pocodeonline/Manager-LPJHT/refs/heads/main/LPJHGMNI.py'
 
 # Bỏ qua SSL verify (nếu cần)
 ssl._create_default_https_context = ssl._create_unverified_context
